1af78602893855de91864849e91a4983bfd9fce4
NathanielNat <psalmnat@gmail.com>
2020-10-29 23:28:14 +0000

    copy paste with pyperclip

---
+++ string_types.py
@@ -0,0 +1,13 @@
+print('hello'.isalpha())
+
+print('hello123'.isalpha())
+
+print('hello123'.isalnum())
+
+print('hello'.isalnum())
+
+print('123'.isdecimal())
+
+print(' '.isspace())
+
+print('Hello'.center(20, '='))
